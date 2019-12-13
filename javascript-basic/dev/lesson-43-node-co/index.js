0d0a36b5e935afaa0ba9f94c36e452156172a2b7
dmhoang <dmhoang08t1@gmail.com>
2019-12-13 16:39:37 +0700

    Node co

--- javascript-basic/dev/lesson-43-node-co/index.js
+++ javascript-basic/dev/lesson-43-node-co/index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var co = require('co');
 
 function readFilePromise(path){
   return new Promise(function(resolve, reject){
@@ -11,3 +12,27 @@ function readFilePromise(path){
     }); 
   });
 }
+
+/* co(function *(){
+  // yield any promise
+  var song1 = yield readFilePromise('./song1.txt');
+  var song2 = yield readFilePromise('./song2.txt');
+  var song3 = yield readFilePromise('./song3.txt');
+  return [song1, song2, song3]
+}).then(function(values){
+  console.log(values);
+}).catch(function(err){
+  console.log(err);
+}) */
+
+var readFile = co.wrap(function* (files) {
+  var values = yield files.map(function(file){
+    return readFilePromise(file)
+  });
+  return values;
+});
+
+readFile(['song1.txt', 'song2.txt', 'song3.txt'])
+  .then(function(values){
+    console.log(values);
+  })

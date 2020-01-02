f18d8ced770a2af65e99eb13ac787494e3f58cd4
dmhoang <dmhoang08t1@gmail.com>
2020-01-02 17:07:14 +0700

    node first install express

--- NodeJS-Web-Server-using-Express-2020/lesson-1-Hello/index.js
+++ NodeJS-Web-Server-using-Express-2020/lesson-1-Hello/index.js
@@ -9,3 +9,27 @@ app.get('/', function (request, response) {
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 })
+
+/* 
+
+javascript là một ngôn ngữ lập trình
+
+- nodejs là một flatform cho js chạy trên server
+
+- express là một frameword của nodejs
+
+- express la một frameword của nodejs, la mot node module, ngon ngu la javascript, 
+
+nen tang de chay la nodejs, la mo dynamic webserver
+
+- Static server: server tra ve cho client theo huong client yeu cau gi thi tra ve cai do, 
+neu ko tim thay thi tra ve loi 404
+
+- Dynamic server: tuong tac voi database, server nhan tham so tu client
+
+1. Liệt kê các bước để setup 1 Project sử dụng Express
+B1: npm init
+B2: npm install express --save
+B3: node index.js
+
+*/

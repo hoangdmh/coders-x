844740e9582a47735e2d91928a40afd68fa375a2
dmhoang <dmhoang08t1@gmail.com>
2020-01-15 13:55:05 +0700

    user middleware

--- NodeJS-Web-Server-using-Express-2020/lesson-10-Static-files/routers/user.router.js
+++ NodeJS-Web-Server-using-Express-2020/lesson-10-Static-files/routers/user.router.js
@@ -3,6 +3,9 @@ var express = require('express');
 // import module controller
 var controller = require("../controller/user.controller");
 
+//import module validate
+var validate = require('../validate/user.validate')
+
 var router = express.Router();
 
 router.get('/', controller.index);
@@ -14,6 +17,6 @@ router.get('/create', controller.create);
 router.get('/:id', controller.get)
 
 // POST data
-router.post('/create', controller.postCreate)
+router.post('/create', validate.postCreate, controller.postCreate)
 
 module.exports = router;

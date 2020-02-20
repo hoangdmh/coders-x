4051c9761cd8b8d066648de0e173c4b4e0c044f5
dmhoang <dmhoang08t1@gmail.com>
2020-02-20 13:55:17 +0700

    inheritance in javascript

--- Javascript-advanced-2020/class-in-javascript.js
+++ Javascript-advanced-2020/class-in-javascript.js
@@ -23,3 +23,44 @@ class MyFun {
 var x = new MyFun(3,4);
  
 x.getSum();// return 7
+
+
+// inheritance in javascript
+class Employee {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  setName(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+  setAge(age) {
+    this.age = age;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+class MaleEmployee extends Employee {
+  constructor(name, age, wifeName) {
+    super(name, age);
+    this.wifeName = wifeName;
+  }
+  setWifeName(wifeName) {
+    this.wifeName = wifeName;
+  }
+  getWifeName() {
+    return this.wifeName;
+  }
+}
+
+var tai = new Employee("Vũ Thanh Tài", 22);
+console.log(typeof tai);
+var trinh = new MaleEmployee("Nam", 23, "Trinh");
+console.log(trinh.getWifeName());
+console.log(trinh.getName());
+console.log(trinh.getAge());
